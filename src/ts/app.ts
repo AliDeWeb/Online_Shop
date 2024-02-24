19c6d04429f83f1b5cd9de05cfdf008c84bde184
Ali Moradi <Alimoradi0business@gmail.com>
2024-02-24 18:19:43 -0300

    project structure completed

---
+++ src/ts/app.ts
@@ -0,0 +1,49 @@
+const productsWrapperSelector = document.querySelector(
+  `.main__products-wrapper`
+)!;
+const fragment = document.createDocumentFragment();
+
+window.addEventListener(`load`, async () => {
+  const data: any = await fetch(
+    `https://65da4c6cbcc50200fcdcd8ca.mockapi.io/products`
+  );
+
+  const dataResult = await data.json();
+  const dataResponse: {
+    id: number;
+    title: string;
+    dec: string;
+    img: string;
+  }[] = await dataResult;
+
+  dataResponse.forEach(
+    (el: { id: number; title: string; dec: string; img: string }) => {
+      let newProduc: HTMLDivElement = document.createElement(`div`);
+      newProduc.classList.add(`col-lg-4`);
+      newProduc.classList.add(`col-md-6`);
+      newProduc.classList.add(`col-12`);
+
+      newProduc.innerHTML = `
+        <div class="position-relative card">
+          <img
+            src=${el.img}
+            class="card-img-top"
+            alt="img"
+            loading="lazy"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${el.title}</h5>
+            <p class="card-text">
+              ${el.dec}
+            </p>
+            <button class="product-btn btn btn-primary position-absolute">Add</button>
+          </div>
+        </div>
+    `;
+
+      fragment.append(newProduc);
+    }
+  );
+
+  productsWrapperSelector.append(fragment);
+});
