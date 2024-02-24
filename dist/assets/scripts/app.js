eaf7ee002b67b1f4b5660809ab4793777c9afba4
Ali Moradi <Alimoradi0business@gmail.com>
2024-02-24 20:14:51 -0300

    pagination feature added

--- dist/assets/scripts/app.js
+++ dist/assets/scripts/app.js
@@ -1,11 +1,48 @@
 "use strict";
 const productsWrapperSelector = document.querySelector(`.main__products-wrapper`);
-const fragment = document.createDocumentFragment();
+const paginationWrapperSelector = document.querySelector(`.pagination-wrapper`);
+const productsFragment = document.createDocumentFragment();
+const paginationFragment = document.createDocumentFragment();
+let datas;
 window.addEventListener(`load`, async () => {
     const data = await fetch(`https://65da4c6cbcc50200fcdcd8ca.mockapi.io/products`);
     const dataResult = await data.json();
     const dataResponse = await dataResult;
-    dataResponse.forEach((el) => {
+    datas = dataResponse;
+    paginationFeature(productsWrapperSelector, paginationWrapperSelector, 6, 1, datas);
+    document.querySelector(`.pagination-btn`).classList.add(`active`);
+});
+const paginationFeature = (itemsWrapper, paginationWrapper, itemsNum, currentPage, itemsArray) => {
+    itemsWrapper.innerHTML = "";
+    if (!paginationWrapper.innerHTML) {
+        let paginationNums = itemsArray.length / itemsNum;
+        for (let i = 0; i <= paginationNums; i++) {
+            let newEl = document.createElement(`span`);
+            newEl.classList.add(`pagination-btn`);
+            newEl.classList.add(`rounded-4`);
+            newEl.classList.add(`bg-dark`);
+            newEl.classList.add(`text-white`);
+            newEl.classList.add(`d-flex`);
+            newEl.classList.add(`justify-content-center`);
+            newEl.classList.add(`align-items-center`);
+            let num = i + 1;
+            newEl.innerHTML = num.toString();
+            newEl.addEventListener(`click`, (e) => {
+                let allActiveItems = document.querySelectorAll(`.pagination-btn.active`);
+                allActiveItems.forEach((el) => el.classList.remove(`active`));
+                let el = e.target;
+                el.classList.add(`active`);
+                paginationFeature(productsWrapperSelector, paginationWrapperSelector, 6, Number(el.innerHTML), datas);
+            });
+            paginationFragment.append(newEl);
+        }
+    }
+    paginationWrapper.appendChild(paginationFragment);
+    let firstIndex = itemsNum * currentPage - itemsNum;
+    let lastIndex = itemsNum * currentPage;
+    itemsArray
+        .slice(firstIndex, lastIndex)
+        .forEach((el) => {
         let newProduc = document.createElement(`div`);
         newProduc.classList.add(`col-lg-4`);
         newProduc.classList.add(`col-md-6`);
@@ -27,7 +64,7 @@ window.addEventListener(`load`, async () => {
           </div>
         </div>
     `;
-        fragment.append(newProduc);
+        productsFragment.append(newProduc);
     });
-    productsWrapperSelector.append(fragment);
-});
+    itemsWrapper.append(productsFragment);
+};
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,13 +1,17 @@
 const productsWrapperSelector = document.querySelector(
   `.main__products-wrapper`
 )!;
-const fragment = document.createDocumentFragment();
+const paginationWrapperSelector =
+  document.querySelector(`.pagination-wrapper`)!;
+const productsFragment = document.createDocumentFragment();
+const paginationFragment = document.createDocumentFragment();
+
+let datas: any;
 
 window.addEventListener(`load`, async () => {
   const data: any = await fetch(
     `https://65da4c6cbcc50200fcdcd8ca.mockapi.io/products`
   );
-
   const dataResult = await data.json();
   const dataResponse: {
     id: number;
@@ -16,8 +20,69 @@ window.addEventListener(`load`, async () => {
     img: string;
   }[] = await dataResult;
 
-  dataResponse.forEach(
-    (el: { id: number; title: string; dec: string; img: string }) => {
+  datas = dataResponse;
+
+  paginationFeature(
+    productsWrapperSelector,
+    paginationWrapperSelector,
+    6,
+    1,
+    datas
+  );
+
+  document.querySelector(`.pagination-btn`)!.classList.add(`active`);
+});
+
+const paginationFeature = (
+  itemsWrapper: Element,
+  paginationWrapper: Element,
+  itemsNum: number,
+  currentPage: number,
+  itemsArray: any
+) => {
+  itemsWrapper.innerHTML = "";
+
+  if (!paginationWrapper.innerHTML) {
+    let paginationNums: number = itemsArray.length / itemsNum;
+    for (let i = 0; i <= paginationNums; i++) {
+      let newEl = document.createElement(`span`);
+      newEl.classList.add(`pagination-btn`);
+      newEl.classList.add(`rounded-4`);
+      newEl.classList.add(`bg-dark`);
+      newEl.classList.add(`text-white`);
+      newEl.classList.add(`d-flex`);
+      newEl.classList.add(`justify-content-center`);
+      newEl.classList.add(`align-items-center`);
+      let num = i + 1;
+      newEl.innerHTML = num.toString();
+      newEl.addEventListener(`click`, (e) => {
+        let allActiveItems = document.querySelectorAll(
+          `.pagination-btn.active`
+        )!;
+        allActiveItems.forEach((el) => el.classList.remove(`active`));
+        let el = e.target as Element;
+        el.classList.add(`active`);
+
+        paginationFeature(
+          productsWrapperSelector,
+          paginationWrapperSelector,
+          6,
+          Number(el.innerHTML),
+          datas
+        );
+      });
+      paginationFragment.append(newEl);
+    }
+  }
+
+  paginationWrapper.appendChild(paginationFragment);
+
+  let firstIndex: number = itemsNum * currentPage - itemsNum;
+  let lastIndex: number = itemsNum * currentPage;
+
+  itemsArray
+    .slice(firstIndex, lastIndex)
+    .forEach((el: { id: number; title: string; dec: string; img: string }) => {
       let newProduc: HTMLDivElement = document.createElement(`div`);
       newProduc.classList.add(`col-lg-4`);
       newProduc.classList.add(`col-md-6`);
@@ -41,9 +106,7 @@ window.addEventListener(`load`, async () => {
         </div>
     `;
 
-      fragment.append(newProduc);
-    }
-  );
-
-  productsWrapperSelector.append(fragment);
-});
+      productsFragment.append(newProduc);
+    });
+  itemsWrapper.append(productsFragment);
+};
